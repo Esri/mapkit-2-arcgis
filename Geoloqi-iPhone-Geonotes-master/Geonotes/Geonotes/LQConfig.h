85ad6a83fd812648cbcd0bacbd8f37c8354d2882
Al Pascual <alpascual@gmail.com>
2013-03-26 15:51:56 -0700

    Ready for making it public

--- Geoloqi-iPhone-Geonotes-master/Geonotes/Geonotes/LQConfig.h
+++
@@ -1,8 +0,0 @@
-
-// Create an application on the Geoloqi developers site and
-// enter your API Key and Secret here.
-// https://developers.geoloqi.com/
-
-static NSString *const LQ_APIKey = @"";
-static NSString *const LQ_APISecret = @"";
-
